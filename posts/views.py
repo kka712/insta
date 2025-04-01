4e397016d23de6b27e1e744126079df78e408e65
kka712 <rlska213@naver.com>
2025-04-01 10:44:58 +0900

    Comment create 기능 구현

--- posts/views.py
+++ posts/views.py
@@ -1,14 +1,15 @@
 from django.shortcuts import render, redirect
 from .models import Post
-from .forms import PostForm
+from .forms import PostForm, CommentForm
 from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 def index(request):
     posts = Post.objects.all()
-
+    form = CommentForm()
     context = {
         'posts': posts,
+        'form': form,
     }
 
     return render(request, 'index.html', context)
@@ -31,3 +32,15 @@ def create(request):
         }
 
         return render(request, 'create.html', context)
+
+
+@login_required
+def comment_create(request, post_id):
+    form = CommentForm(request.POST)
+
+    if form.is_valid():
+        comment = form.save(commit=False)
+        comment.user = request.user
+        comment.post_id = post_id
+        comment.save()
+        return redirect('posts:index')
